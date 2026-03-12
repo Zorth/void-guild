0b020850a255e30d1226acab6e72d5122c45795e
Zorth Thorch <jasper_goens@hotmail.com>
2026-03-12 03:16:07 +0100

    chore: update generated convex api types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,6 +8,7 @@
  * @module
  */
 
+import type * as activity from "../activity.js";
 import type * as characters from "../characters.js";
 import type * as crons from "../crons.js";
 import type * as planning from "../planning.js";
@@ -21,6 +22,7 @@ import type {
 } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  activity: typeof activity;
   characters: typeof characters;
   crons: typeof crons;
   planning: typeof planning;
