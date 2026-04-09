2f8b915ac09068eef8d6ba40288d85bda0256223
Zorth Thorch <jasper_goens@hotmail.com>
2026-04-09 20:30:03 +0200

    missing /div

--- components/sessions/details/SessionManagement.tsx
+++ components/sessions/details/SessionManagement.tsx
@@ -189,6 +189,7 @@ export default function SessionManagement({
                 </div>
             </DialogContent>
         </Dialog>
+    </div>
         
         <SessionDialog 
             session={session} 
